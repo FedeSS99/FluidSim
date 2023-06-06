a919f18485b38e007819d9c52f0f238e1b3be342
Federico <salinasfederico773@gmail.com>
2023-06-06 12:15:45 -0600

    Edit TimeStep files

--- TimeStep.cpp
+++ TimeStep.cpp
@@ -12,20 +12,20 @@ float TimeStepEq(ScalarArrays *Scalars, float Diff, int index){
     return 0.5*(Diff/( sqrtf(gas_c*Pres_ind/Dens_ind) + sqrtf(Vx2_ind + Vy2_ind)));
 }
 
-float GetOptimalTimeStep(ScalarArrays *Scalars, float Diff){
+void GetOptimalTimeStep(float *dt, ScalarArrays *Scalars, float Diff){
     int i,j, index;
-    float dt0 = TimeStepEq(Scalars, Diff, 0);
-    float dt;
+    float dt_ref = TimeStepEq(Scalars, Diff, 0);
+    float dt_index;
     
     for (i=0; i<Ny; i++){
         for (j=0; j<Nx; j++){
             index = GetIndex(j, i, Nx);
 
-            dt = TimeStepEq(Scalars, Diff, index);
-            if (dt < dt0){
-                dt0 = dt;
+            dt_index = TimeStepEq(Scalars, Diff, index);
+            if (dt_index < dt_ref){
+                dt_ref = dt_index;
+                (*dt) = dt_ref;
             } 
         }
     }
-    return dt0;
 }
