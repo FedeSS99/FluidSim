3e094b1182b9fcaac54c3795c045bfa513474d32
Federico <salinasfederico773@gmail.com>
2023-06-07 08:09:26 -0600

    Changed input name in definition

--- Index.h
+++ Index.h
@@ -1,7 +1,7 @@
 #ifndef INDEXR_H
 #define INDEXR_H
 
-extern int GetIndex(int x, int y, int Nx);
+extern int GetIndex(int x, int y, int N);
 extern void GetPeriodicIndex(int *index, int N);
 
 #endif
